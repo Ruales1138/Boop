4b6ff4ac1f0d9d12d6db45f88763890475a6bbfb
David Ruales <hruales1138@gmail.com>
2025-10-30 13:39:12 -0500

    direcciones

--- boop.py
+++ boop.py
@@ -3,20 +3,66 @@ class Boop:
         self.table = []
         self.size = 4
         self.shift = 1
+        self.space = '  '
+        self.kitten_1 = '😈' 
+        self.kitten_2 = '😺'
+        self.pieces_1 = 6
+        self.pieces_2 = 6
 
     def create(self):
         for _ in range(self.size):
-            self.table.append([' '] * self.size)
+            self.table.append([self.space] * self.size)
 
     def print_table(self):
         for i in range(self.size):
             print(self.table[i])
 
-    def move(self, location: tuple):
+    def put(self, location: tuple):
         x, y = location
-        if x - 1 > 0:
-            pass
-        
+        if self.shift == 1:
+            self.table[x][y] = self.kitten_1
+            self.pieces_1 -= 1
+        if self.shift == 2:
+            self.table[x][y] = self.kitten_2
+            self.pieces_2 -= 1
+        # Arriba
+        if x-1 >= 0 and self.table[x-1][y] != self.space:
+            if x-2 >= 0:
+                if self.table[x-2][y] == self.space:
+                    self.table[x-2][y] = self.table[x-1][y]
+                    self.table[x-1][y] = self.space
+            else:
+                self.table[x-1][y] = self.space
+        # Abajo
+        if x+1 < self.size and self.table[x+1][y] != self.space:
+            if x+2 < self.size:
+                if self.table[x+2][y] == self.space:
+                    self.table[x+2][y] = self.table[x+1][y]
+                    self.table[x+1][y] = self.space
+            else:
+                self.table[x+1][y] = self.space
+        # Izquierda
+        if y-1 >= 0 and self.table[x][y-1] != self.space:
+            if y-2 >= 0:
+                if self.table[x][y-2] == self.space:
+                    self.table[x][y-2] = self.table[x][y-1]
+                    self.table[x][y-1] = self.space
+            else:
+                self.table[x][y-1] = self.space
+        # Derecha
+        if y+1 < self.size and self.table[x][y+1] != self.space:
+            if y+2 < self.size:
+                if self.table[x][y+2] == self.space:
+                    self.table[x][y+2] = self.table[x][y+1]
+                    self.table[x][y+1] = self.space
+            else:
+                self.table[x][y+1] = self.space
+
+        if self.shift == 1:
+            self.shift = 2
+        else:
+            self.shift = 1
+
 
 
 
--- console_interface.py
+++ console_interface.py
@@ -5,9 +5,29 @@ class Console:
         self.boop = Boop()
         self.boop.create()
         self.boop.print_table()
+        self.option = 0
 
     def interface(self):
-        pass
-    
+        while self.option != 3:
+            print('--------------------------------------------')
+            if self.boop.shift == 1:
+                pieces = ''
+                for _ in range(self.boop.pieces_1):
+                    pieces += self.boop.kitten_1
+                print(f'Jugador 1 {pieces}')
+            if self.boop.shift == 2:
+                pieces = ''
+                for _ in range(self.boop.pieces_2):
+                    pieces += self.boop.kitten_2
+                print(f'Jugador 2 {pieces}')
+            x = int(input('Ingrese una nueva ubicacion en x:\n'))
+            y = int(input('Ingrese una nueva ubicacion en y:\n'))
+            print(f'Moviendo a ({x}, {y})')
+            self.boop.put((x, y))
+            self.boop.print_table()
+
+            
+
 
 c = Console()
+c.interface()
